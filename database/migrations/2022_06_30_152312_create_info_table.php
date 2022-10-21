ea6a0ba11b96c1e1d117e9e8aa013843bd46730c
alireza_eftekhari <alireza.eftekhari@digikala.com>
2022-10-21 20:46:52 +0330

    migrations added

---
+++ database/migrations/2022_06_30_152312_create_info_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('version');
+            $table->text('version_tag');
+            $table->text('description');
+            $table->text('oem');
+            $table->text('finger_print');
+            $table->text('file_address');
+            $table->timestamp('created_at')->default(date('Y-m-d H:i:s'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info');
+    }
+}
